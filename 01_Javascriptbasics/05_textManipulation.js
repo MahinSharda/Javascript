de265440424d86ce1c41d673bec00b447335b935
MahinSharda <mahinsharda160297@gmail.com>
2024-12-14 20:33:49 +0530

    New code file on strings and it's functions
    This file includes simple functions to work with strings, like trimming, splitting, and extracting parts

---
+++ 01_Javascriptbasics/05_textManipulation.js
@@ -0,0 +1,49 @@
+const userName = "hitesh"; // User's name variable
+const repoCount = 50; // Number of repositories
+
+// Logging user name and repo count using template literals
+console.log(`Hello my name is ${userName} and my repo count is ${repoCount}`);
+
+// Creating a string object for the game name
+const gameDetails = new String("hitesh-hc-com");
+
+// Accessing the first character of the string (commented out to prevent output)
+console.log(gameDetails[0]); // First character of the game name
+
+// Checking the prototype of the string object (commented out as it's not needed for output)
+console.log(gameDetails.__proto__);
+
+// Logging the length of the string and converting it to uppercase (commented out unnecessary outputs)
+console.log(gameDetails.length); // Length of the string
+console.log(gameDetails.toUpperCase()); // Converting the game name to uppercase
+
+// Extracting the character at index 2
+console.log(gameDetails.charAt(2)); // Character at index 2
+
+// Finding the index of character 't' in the string
+console.log(gameDetails.indexOf("t")); // Index of character 't'
+
+// Extracting a substring from the game name (first 4 characters)
+const partialString = gameDetails.substring(0, 4);
+console.log(partialString); // First 4 characters
+
+// Using slice with negative index to extract a portion of the string (starting from the 8th last character to the 4th)
+const slicedString = gameDetails.slice(-8, 4);
+console.log(slicedString); // Portion of the string from the negative index
+
+// String with extra spaces
+const spacedString = "   hitesh    ";
+console.log(spacedString); // String with spaces at both ends
+console.log(spacedString.trim()); // Removing spaces from both ends
+
+// URL string with encoded characters
+const urlString = "https://hitesh.com/hitesh%20choudhary";
+
+// Replacing '%20' with a dash in the URL
+console.log(urlString.replace("%20", "-")); // Replacing encoded space with a dash
+
+// Checking if the string contains the word 'sundar'
+console.log(urlString.includes("sundar")); // Checking for 'sundar'
+
+// Splitting the game name string based on '-'
+console.log(gameDetails.split("-")); // Splitting game name into an array
